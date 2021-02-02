426d09ea21246128f7f217f528f8fe9bb2bdcc1d
anuradhaj <anuradha.jayalath@syscolabs.com>
2021-02-02 22:12:10 +0530

    first commit

---
+++ src/main/java/com/simplycache/InMemoryCache.java
@@ -0,0 +1,15 @@
+package com.simplycache;
+
+/**
+ * @author anuradhaj Date: 2/2/21
+ */
+public class InMemoryCache {
+
+  public String getValue(String key){
+    return "value";
+  }
+
+  public String putValue(String key, String value){
+    return "key-value";
+  }
+}
---
+++ src/test/java/com/simplycache/InMemoryCacheTest.java
@@ -0,0 +1,24 @@
+package com.simplycache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author anuradhaj Date: 2/2/21
+ */
+public class InMemoryCacheTest {
+
+  private final InMemoryCache inMemoryCache = new InMemoryCache();
+
+  @Test
+  public void testCacheGet(){
+    assertEquals("value", inMemoryCache.getValue("key"));
+  }
+
+  @Test
+  public void testCachePut(){
+    assertEquals("key-value", inMemoryCache.putValue("key","value"));
+  }
+
+}
